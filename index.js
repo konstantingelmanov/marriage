89c306bec6981e3549655ee0dc4a12892bf8042e
KonstantinGelmanov <kv.gelmanov@mail.ru>
2025-06-18 06:04:37 +0200

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+    output.innerHTML = 'Желаем долгой и счастливой семейной жизни)) Чмоки чмоки!';
+});
